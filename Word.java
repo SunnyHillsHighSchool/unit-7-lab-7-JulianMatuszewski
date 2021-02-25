84b7e2418dec8d8c3ec61aa53ef816f9d80a98e5
Julian Matuszewski <600018222@fjuhsd.org>
2021-02-25 03:47:33 +0000

    Words class

---
+++ Word.java
@@ -0,0 +1,70 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Julian Matuszewski
+
+import static java.lang.System.*;
+
+public class Word
+{
+  private static String vowels = "AEIOUaeiou";
+	//why is this here? This stores all vowels for reference of the class
+	//why is it static? It is shared by all Word object
+
+  //add an instance variable...
+
+	private String str;
+
+	//create 2 constructors...
+
+	//No Argument Constructor:
+	public Word()
+	{
+		this.str = ""; //empty String
+	}
+
+	//Normal Word Constructor:
+	public Word (String str)
+	{
+		this.str = str;
+	}
+
+	//Get Word
+	public String getWord()
+	{
+		return str;
+	}
+
+	//Get Num Vowels
+	public int getNumVowels ()
+	{
+  	//counter integer
+		int counter = 0;
+ 		//use for loop iterating through str
+		for (int i = 0; i < str.length(); i++) //iterate through all letters in str and
+ 			for (int j = 0; j < vowels.length(); j++) //compare to all letters in vowels
+				//if it matches with a vowel...
+				if (str.charAt(i) == vowels.charAt(j))
+					counter++; //...incr counter
+ 		//return counter
+		return counter;
+	}
+
+	//Get Length
+	public int getLength()
+	{
+  	return str.length();
+	}
+
+	//Set Word
+	public void setWord(String str)
+	{
+		this.str = str;
+	}
+
+	//toString
+	public String toString()
+	{
+  	return str;
+	}
+
+}
--- Words.java
+++ Words.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Julian Matuszewski
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -13,11 +13,24 @@ public class Words
 
 	public Words(String[] wordList)
 	{
+		words = new ArrayList<Word>();
+		for (String str: wordList) {
+			words.add(new Word(str)); //creates new Word for each string in the parameter
+		}
 	}
 
 	public int countWordsWithXChars(int size)
 	{
-		int count=0;
+		//Using getLength from Word class
+		//make int count
+		int count = 0;
+		//for loop iterating through all words in array
+		for (Word w: words)
+			//if size of String = x
+			if (w.getLength() == size)
+				//count++
+				count++;
+		//return count
 		return count;
 	}
 	
@@ -25,14 +38,45 @@ public class Words
 	//this method will also return the sum of the vowels in all words removed
 	public int removeWordsWithXChars(int size)
 	{
-		return 0;
+		//Using getLength from Word class
+		//make int count
+		int count = 0;
+		//for loop iterating through all words in array
+		for (int i = words.size() - 1; i >= 0; i--) //iterates backwards so it doesnt skip any when two elements are changed next to eachother
+		//if size of String = size
+			if (words.get(i).getLength() == size) {
+				//add to sum of the vowels in all words removed
+				count += words.get(i).getNumVowels();
+				//remove element of index
+				words.remove(i);
+			}
+		return count;
 	}
 
 	public int countWordsWithXVowels(int numVowels)
 	{
-		int count=0;
+		//Using getNumVowels from Word class
+		//make int count
+		int count = 0;
+		//for loop iterating through all words in array
+		for (Word w: words)
+			//for loop iterating through each word
+			if (w.getNumVowels() == numVowels)
+				//count++
+				count++;
+		//return count
 		return count;
 	}
 	
 	//add in a toString
+	public String toString () {
+		String str = "[" + words.get(0).toString(); //adds this one outside loop, so it formats correctly
+		String s;
+		for (int i = 1; i < words.size(); i++) { //iterates, adding ", " and word String
+			s = words.get(i).toString();
+			str += ", " + s;
+		}
+		str += "]";
+		return str;
+	}
 }
